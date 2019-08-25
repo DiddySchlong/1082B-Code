138678e3bdce074bc3a62aed2da5866fb3100689
David Logan <47231244+DiddySchlong@users.noreply.github.com>
2019-08-25 12:00:32 -0500

    Add files via upload

--- PaCEP/include/motor.hpp
+++ PaCEP/include/motor.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "main.h"
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
 
 extern Controller master;
 extern Motor leftBack;
@@ -10,3 +12,7 @@ extern Motor tray;
 extern Motor intakeLeft;
 extern Motor intakeRight;
 extern Motor arm;
+int trayPID(struct tray* pid, float kP, float kI, float kD);
+float PIDUpdate(struct tray* pid, float error, float dT);
+
+#endif
